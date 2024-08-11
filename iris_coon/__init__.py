ec3a959418d7be79636e17b62db930bce40aa14f
Maki <sunwood.ai.labs@gmail.com>
2024-08-11 16:03:53 +0900

    ✨ [feat] Iris Coonパッケージの初期化
    - パッケージのメタ情報(__description__, __author__, __author_email__, __license__)を設定しました。
    - IrisCoonクラスを公開し、外部から利用できるようにしました。

---
+++ iris_coon/__init__.py
@@ -0,0 +1,13 @@
+from .core import IrisCoon
+
+__all__ = ['IrisCoon']
+
+# パッケージの説明
+__description__ = "IRISリポジトリをクローンし、.githubフォルダをコピーするパッケージ"
+
+# 作者情報
+__author__ = "Maki"
+__author_email__ = "sunwood.ai.labs@gmail.com"
+
+# ライセンス情報
+__license__ = "MIT"
